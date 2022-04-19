76871623361c60f888d05e0d30bfb3df77d74ad5
Diego Ayala <dieguito9677@gmail.com>
2022-04-19 16:23:58 -0400

    finish 3.0

--- config/settings.py
+++ config/settings.py
@@ -82,8 +82,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'penaval_system',
+        'USER': 'postgres',
+        'PASSWORD': 'Telecom2',
+        'HOST': 'localhost',
+        'PORT': 5432,
+
     }
 }
 
